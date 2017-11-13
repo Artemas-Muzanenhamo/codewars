5b6bc89f65b076f7f9d514a9335dfca9129cbcca
Artemas-Muzanenhamo <artemasseng@gmail.com>
2017-11-13 19:08:38 +0000

    Completed Counter Kata

---
+++ src/test/java/com/artemas/codewars/fundamentals/CounterTest.java
@@ -0,0 +1,20 @@
+package com.artemas.codewars.fundamentals;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.runners.JUnit4;
+
+
+public class CounterTest {
+    Boolean[] array1 = {true,  true,  true,  false,
+                      true,  true,  true,  true ,
+                      true,  false, true,  false,
+                      true,  false, false, true ,
+                      true,  true,  true,  true ,
+                      false, false, true,  true };
+
+    @Test
+    public void test() {
+      assertEquals("There are 17 sheeps in total", 17, new Counter().countSheeps(array1));
+    }
+}
