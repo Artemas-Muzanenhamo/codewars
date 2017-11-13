b57eea8f805704931ea61b969422fd8e3dc76729
Artemas-Muzanenhamo <artemasseng@gmail.com>
2017-11-13 16:51:13 +0000

    Completed the NumberToString Kata

---
+++ src/main/java/com/artemas/codewars/fundamentals/NumberStringExample.java
@@ -0,0 +1,7 @@
+package com.artemas.codewars.fundamentals;
+
+class NumberStringExample {
+  public static String numberToString(int num) {
+    return String.valueOf(num);
+  }
+}
---
+++ src/test/java/com/artemas/codewars/fundamentals/NumberStringExampleTests.java
@@ -0,0 +1,13 @@
+package com.artemas.codewars.fundamentals;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class NumberStringExampleTests {
+  @Test
+  public void tests() {
+    assertEquals("67", NumberStringExample.numberToString(67));
+    assertEquals("123", NumberStringExample.numberToString(123));
+    assertEquals("999", NumberStringExample.numberToString(999));
+  }
+}
