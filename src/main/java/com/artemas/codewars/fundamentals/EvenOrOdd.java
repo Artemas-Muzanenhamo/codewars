8e8308e3f84eea4b5ddf9fbe5851f1713844bc84
Artemas <artemasseng@gmail.com>
2017-12-20 13:31:08 +0000

    Add EvenOrOdd solution

---
+++ src/main/java/com/artemas/codewars/fundamentals/EvenOrOdd.java
@@ -0,0 +1,11 @@
+package com.artemas.codewars.fundamentals;
+
+public class EvenOrOdd {
+    public static String even_or_odd(int number) {
+        if (number % 2 == 0){
+            return "Even";
+        }else {
+            return "Odd";
+        }
+    }
+}
