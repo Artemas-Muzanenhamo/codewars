8c2d2d1e42ef523136afba51ff058f2d22e5b93c
Artemas <artemasseng@gmail.com>
2017-12-22 02:44:32 +0000

    Add Welcome solution

---
+++ src/main/java/com/artemas/codewars/fundamentals/Welcome.java
@@ -0,0 +1,36 @@
+package com.artemas.codewars.fundamentals;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Welcome {
+   public static String greet(String language){
+      return getWelcomeMessage(language);
+   }
+
+   public static String getWelcomeMessage(String language) {
+
+      Map<String, String> welcomeMessage = new HashMap<>();
+      
+      welcomeMessage.put("english", "Welcome");
+      welcomeMessage.put("czech", "Vitejte");
+      welcomeMessage.put("danish", "Velkomst");
+      welcomeMessage.put("dutch", "Welkom");
+      welcomeMessage.put("estonian", "Tere tulemast");
+      welcomeMessage.put("finnish", "Tervetuloa");
+      welcomeMessage.put("flemish", "Welgekomen");
+      welcomeMessage.put("french", "Bienvenue");
+      welcomeMessage.put("german", "Willkommen");
+      welcomeMessage.put("irish", "Failte");
+      welcomeMessage.put("italian", "Benvenuto");
+      welcomeMessage.put("latvian", "Gaidits");
+      welcomeMessage.put("lithuanian", "Laukiamas");
+      welcomeMessage.put("polish", "Witamy");
+      welcomeMessage.put("spanish", "Bienvenido");
+      welcomeMessage.put("swedish", "Valkommen");
+      welcomeMessage.put("welsh", "Croeso");
+      welcomeMessage.put("IP_ADDRESS_INVALID", "Welcome");
+
+      return welcomeMessage.get(language);
+   }
+}
---
+++ src/test/java/com/artemas/codewars/fundamentals/WelcomeTest.java
@@ -0,0 +1,14 @@
+package com.artemas.codewars.fundamentals;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.runners.JUnit4;
+
+public class WelcomeTest {
+    @Test
+    public void exampleTest() {
+        assertEquals("Your function should have returned 'Welcome'. Try again.","Welcome", Welcome.greet("english"));
+        assertEquals("Your function should have returned 'Welkom'. Try again.", "Welkom", Welcome.greet("dutch"));
+        assertEquals("Your function should have returned 'Welcome'. Try again.", "Welcome", Welcome.greet("IP_ADDRESS_INVALID"));
+    }
+}
