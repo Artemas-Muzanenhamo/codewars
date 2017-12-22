cbec71a3deee9e1d3e892122b5bcffc3e588fd51
Artemas <artemasseng@gmail.com>
2017-12-22 10:12:08 +0000

    Add ToString solution

---
+++ src/main/java/com/artemas/codewars/fundamentals/ToString.java
@@ -0,0 +1,6 @@
+package com.artemas.codewars.fundamentals;
+
+public class ToString {
+
+  public static final String a = String.valueOf(123);
+}
---
+++ src/test/java/com/artemas/codewars/fundamentals/ToStringTest.java
@@ -0,0 +1,14 @@
+package com.artemas.codewars.fundamentals;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.runners.JUnit4;
+
+// TODO: Replace examples and use TDD development by writing your own tests
+
+public class ToStringTest {
+    @Test
+    public void testSomething() {
+        assertEquals("123", ToString.a);
+    }
+}
