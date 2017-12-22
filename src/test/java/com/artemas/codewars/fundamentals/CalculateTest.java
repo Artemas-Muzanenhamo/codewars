e4420a1d062a3d12caec74abd55b230c5cd12b7f
Artemas <artemasseng@gmail.com>
2017-12-22 00:02:52 +0000

    Add Calculate solution

---
+++ src/test/java/com/artemas/codewars/fundamentals/CalculateTest.java
@@ -0,0 +1,12 @@
+package com.artemas.codewars.fundamentals;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.runners.JUnit4;
+
+public class CalculateTest {
+    @Test
+    public void testBMI() {
+        assertEquals("Normal", Calculate.bmi(80, 1.80));
+    }
+}
