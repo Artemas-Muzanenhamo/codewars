5d7d849bf215db9c4638b55f3b628e47b93911f5
Artemas <artemasseng@gmail.com>
2017-12-22 11:27:03 +0000

    Add Number solution

---
+++ src/main/java/com/artemas/codewars/fundamentals/Number.java
@@ -0,0 +1,8 @@
+package com.artemas.codewars.fundamentals;
+
+public class Number {
+
+  public boolean isEven(double n) {
+    return (n % 2 == 0);
+  }
+}
---
+++ src/test/java/com/artemas/codewars/fundamentals/NumberTest.java
@@ -0,0 +1,29 @@
+package com.artemas.codewars.fundamentals;
+
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+import static org.junit.Assert.assertEquals;
+
+public class NumberTest {
+  private Number num;
+  
+  @Before
+  public void setUp() throws Exception {
+    num = new Number();
+  }
+
+  @After
+  public void setDown() throws Exception {
+    num = null;
+  }
+  
+  @Test
+  public void test() {
+    assertEquals(true, num.isEven(0));
+    assertEquals(false, num.isEven(0.5));
+    assertEquals(false, num.isEven(1));
+    assertEquals(true, num.isEven(2));
+    assertEquals(true, num.isEven(-4)); 
+  }
+}
