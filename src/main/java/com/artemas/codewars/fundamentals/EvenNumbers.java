37b3d36c49ca670fa9d0f3718d49a7cc0947340b
Artemas <artemasseng@gmail.com>
2017-12-22 01:45:51 +0000

    Add Even Numbers solution

---
+++ src/main/java/com/artemas/codewars/fundamentals/EvenNumbers.java
@@ -0,0 +1,11 @@
+package com.artemas.codewars.fundamentals;
+
+import java.util.Arrays;
+
+public class EvenNumbers {
+  public static int[] divisibleBy(int[] numbers, int divider) {
+
+    return Arrays.stream(numbers).filter(n -> n % divider == 0).toArray();
+
+  }
+}
