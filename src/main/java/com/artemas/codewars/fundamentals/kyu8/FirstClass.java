2a5eb3b65ce9f9affb9a6cf8e22927b71d3e8638
Artemas <artemasseng@gmail.com>
2018-01-01 15:34:05 +0000

    Add FirstClass solution

---
+++ src/main/java/com/artemas/codewars/fundamentals/kyu8/FirstClass.java
@@ -0,0 +1,8 @@
+package com.artemas.codewars.fundamentals.kyu8;
+
+public class FirstClass {
+    public static byte sum (byte a, byte b) {
+        byte c = (byte)(a + b);
+        return c;
+    }
+}
---
+++ src/test/java/com/artemas/codewars/fundamentals/kyu8/FirstClassTest.java
@@ -0,0 +1,13 @@
+package com.artemas.codewars.fundamentals.kyu8;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class FirstClassTest {
+    @Test
+    public void testSum() throws Exception {
+        byte a = 1;
+        byte b = 2;
+        assertEquals(3, FirstClass.sum(a, b));
+    }
+}
