a206ea6d05cd2b1837a33465b3a382f74f8be3fc
Artemas <artemasseng@gmail.com>
2018-01-01 15:24:58 +0000

    Polish up

--- src/main/java/com/artemas/codewars/fundamentals/kyu8/Kata13December.java
+++ src/main/java/com/artemas/codewars/fundamentals/kyu8/Kata13December.java
@@ -4,17 +4,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Kata13December {
-    public static List<Integer> filterOddNumber(List<Integer> listOfNumbers)
-    {
-//        for (int i = 0; i < listOfNumbers.size(); i++)
-//        {
-//            if (listOfNumbers.get(i)%2 == 0)
-//            {
-//                listOfNumbers.remove(i);
-//            }
-//        }
-//        return listOfNumbers;
-
+    public static List<Integer> filterOddNumber(List<Integer> listOfNumbers) {
         return listOfNumbers.stream().filter(n -> n%2 != 0).collect(Collectors.toList());
     }
 }
