847b6f8bc69e1e2d4db4ca437034ff81896cbb5d
Artemas <artemasseng@gmail.com>
2018-01-15 23:18:05 +0000

    Add SaleHotdogs solution

---
+++ src/main/java/com/artemas/codewars/fundamentals/kyu8/SaleHotdogs.java
@@ -0,0 +1,7 @@
+package com.artemas.codewars.fundamentals.kyu8;
+
+public class SaleHotdogs{
+  public static int saleHotdogs(final int n){
+    return (n < 5) ? n * 100 : (n >= 5 && n < 10) ? n * 95 : n * 90;
+  }
+}
