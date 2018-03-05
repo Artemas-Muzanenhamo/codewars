5c713a5ee8f9bf58be6468201e94c11fef626f00
Artemas-Muzanenhamo <artemasseng@gmail.com>
2018-03-05 10:37:24 +0000

    polish up

--- src/main/java/com/artemas/codewars/fundamentals/kyu8/Num.java
+++ src/main/java/com/artemas/codewars/fundamentals/kyu8/Num.java
@@ -2,6 +2,6 @@ package com.artemas.codewars.fundamentals.kyu8;
 
 public class Num {
   public static int nthEven(int n) {
-      return (n * 2) - 2;
+      return (n * 2) - 2; // or (n - 1) * 2
   }
 }
