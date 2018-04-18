f3a61d2bd39cf59c62994847edd08f145fe0f9b8
Artemas-Muzanenhamo <artemasseng@gmail.com>
2018-04-18 10:24:14 +0100

    add Functional Programming Solution

---
+++ src/main/java/com/artemas/codewars/fundamentals/kyu6/AggregationKata.java
@@ -0,0 +1,14 @@
+package com.artemas.codewars.fundamentals.kyu6;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class AggregationKata {
+
+    public static Map<String, List<String>> getStudentNamesByDepartment(Stream<Student> students) {
+        return students.collect(Collectors.groupingBy(Student::getDepartment, Collectors.mapping(Student::getName, Collectors.toList())));
+    }
+
+}
