fbdf6f1664afec66d74f981eba58a76d405c3645
Artemas-Muzanenhamo <artemasseng@gmail.com>
2018-04-18 10:58:21 +0100

    polish up

--- src/main/java/com/artemas/codewars/fundamentals/kyu6/AggregationKata3.java
+++ src/main/java/com/artemas/codewars/fundamentals/kyu6/AggregationKata3.java
@@ -2,13 +2,19 @@ package com.artemas.codewars.fundamentals.kyu6;
 
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.*;
 
 public class AggregationKata3 {
 
     public static Map<String, List<String>> getStudentNamesByDepartment(Stream<Student> students) {
-        return students.collect(Collectors.groupingBy(Student::getDepartment, Collectors.mapping(Student::getName, Collectors.toList())));
+        return students.collect(
+                groupingBy(
+                        Student::getDepartment,
+                        mapping(Student::getName, toList())
+                )
+        );
     }
 
 }
