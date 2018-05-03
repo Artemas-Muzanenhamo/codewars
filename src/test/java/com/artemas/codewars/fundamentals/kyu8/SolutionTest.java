23ac6e5d96b8cdf37a2fae731acc935937fa9f62
Artemas-Muzanenhamo <artemasseng@gmail.com>
2018-05-03 14:17:42 +0100

    polish up

--- src/test/java/com/artemas/codewars/fundamentals/kyu8/SolutionTest.java
+++ src/test/java/com/artemas/codewars/fundamentals/kyu8/SolutionTest.java
@@ -2,9 +2,6 @@ package com.artemas.codewars.fundamentals.kyu8;
 
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
-import org.junit.runners.JUnit4;
-
-
 
 public class SolutionTest {
     @Test
