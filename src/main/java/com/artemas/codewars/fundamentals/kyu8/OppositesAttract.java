f561909f5df19e2815e502ee8f7a49c7a82d9ec0
Artemas-Muzanenhamo <artemasseng@gmail.com>
2018-09-11 21:30:06 +0100

    Add solution for OppositesAttract

---
+++ src/main/java/com/artemas/codewars/fundamentals/kyu8/OppositesAttract.java
@@ -0,0 +1,9 @@
+package com.artemas.codewars.fundamentals.kyu8;
+
+public class OppositesAttract {
+
+  public static boolean isLove(final int flower1, final int flower2) {
+    return (flower1 % 2 == 0 && flower2 % 2 != 0) || (flower1 % 2 != 0 && flower2 % 2 == 0);
+  }
+  
+}
