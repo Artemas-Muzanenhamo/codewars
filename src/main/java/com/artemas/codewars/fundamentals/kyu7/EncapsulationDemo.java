910f20058e243e0a7449e15be39717c31a68c7d2
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2019-02-04 13:42:24 +0000

    amend EncapsulationDemo

--- src/main/java/com/artemas/codewars/fundamentals/kyu7/EncapsulationDemo.java
+++ src/main/java/com/artemas/codewars/fundamentals/kyu7/EncapsulationDemo.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class EncapsulationDemo{
+class EncapsulationDemo{
   private int number;
   private String stringValue;
   private Object anObject;
--- src/test/java/com/artemas/codewars/fundamentals/kyu7/EncapsulationDemoTest.java
+++ src/test/java/com/artemas/codewars/fundamentals/kyu7/EncapsulationDemoTest.java
@@ -1,10 +1,8 @@
 package com.artemas.codewars.fundamentals.kyu7;
 
 import org.junit.Test;
+
 import static org.junit.Assert.assertEquals;
-import org.junit.runners.JUnit4;
-import java.security.SecureRandom;
-import java.math.BigInteger;
 
 public class EncapsulationDemoTest {
     @Test
