5de30280c7b81ea9ab7a5ec89ae2ec0acf08abd0
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2021-08-16 10:56:39 +0100

    clean up

--- src/main/java/com/artemas/codewars/fundamentals/kyu8/FirstNonConsecutive.java
+++ src/main/java/com/artemas/codewars/fundamentals/kyu8/FirstNonConsecutive.java
@@ -1,17 +1,25 @@
 package com.artemas.codewars.fundamentals.kyu8;
 
+import java.util.stream.IntStream;
+
 class FirstNonConsecutive {
     static Integer find(final int[] array) {
-        if (array.length == 0 || array.length == 1) {
-            return null;
-        }
-
-        for (int i = 1; i < array.length; i++) {
-            if (array[i] - (array[i - 1]) > 1) {
-                return array[i];
-            }
-        }
-
-        return null;
+//        if (array.length == 0 || array.length == 1) {
+//            return null;
+//        }
+//
+//        for (int i = 1; i < array.length; i++) {
+//            if (array[i] - (array[i - 1]) > 1) {
+//                return array[i];
+//            }
+//        }
+//
+//        return null;
+        return IntStream
+                .range(1, array.length)
+                .filter(i -> array[i] - array[i - 1] != 1)
+                .mapToObj(i -> array[i])
+                .findFirst()
+                .orElse(null);
     }
 }
