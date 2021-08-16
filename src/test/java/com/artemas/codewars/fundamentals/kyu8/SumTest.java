7e6c23f1b1fea57b18ccc1498c52ce8b315be81b
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2021-08-16 10:04:40 +0100

    clean up

--- src/test/java/com/artemas/codewars/fundamentals/kyu8/SumTest.java
+++ src/test/java/com/artemas/codewars/fundamentals/kyu8/SumTest.java
@@ -4,8 +4,6 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import org.junit.runners.JUnit4;
 
-// TODO: Replace examples and use TDD development by writing your own tests
-
 public class SumTest {
     @Test
     public void sampleTests() {
